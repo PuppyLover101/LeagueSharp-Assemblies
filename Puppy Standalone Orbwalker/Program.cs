580c08a488993951790ab31fd844e3feb39eb8bf
CYCOABHI <s.abhirup@yahoo.com>
2015-11-22 21:03:24 -0500

    Standalone Orbwalker

---
+++ Puppy Standalone Orbwalker/Program.cs
@@ -0,0 +1,54 @@
+﻿#region
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using LeagueSharp;
+using LeagueSharp.Common;
+using SharpDX;
+#endregion
+namespace Puppy_Standalone_Orbwalker
+{
+    internal class Program
+    {
+        public const string ChampionName = "balbalbal";
+        static Orbwalking.Orbwalker Orbwalker;
+        static Menu Menu;
+        public static Spell Q;
+        public static Spell W;
+        public static Spell E;
+        public static Spell R;
+        private static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
+        }
+
+        private static void Game_OnGameLoad(EventArgs args)
+        {
+            Menu = new Menu("Puppy Standalone Orbwalker", ObjectManager.Player.ChampionName, true);
+
+            Menu OrbwalkerMenu = Menu.AddSubMenu(new Menu("Orbwalker", "Orbwalker"));
+            Orbwalker = new Orbwalking.Orbwalker(OrbwalkerMenu);
+
+            Menu TargetSelectorMenu = Menu.AddSubMenu(new Menu("Target Selector", "TargetSelector"));
+            TargetSelector.AddToMenu(TargetSelectorMenu);
+
+            Menu.AddToMainMenu();
+            Q = new Spell(SpellSlot.Q, 1);
+            W = new Spell(SpellSlot.W, 1);
+            E = new Spell(SpellSlot.E, 1);
+            R = new Spell(SpellSlot.R, 1);
+          
+            Game.OnUpdate += Game_OnGameUpdate;
+
+        }
+        private static void Game_OnGameUpdate(EventArgs args)
+        {
+            switch (Orbwalker.ActiveMode)
+            {
+                case Orbwalking.OrbwalkingMode.Combo:
+         
+                    break;
+            }
+        }
+    }
+}
