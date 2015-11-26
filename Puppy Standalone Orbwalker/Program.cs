7e00cbbffab96af1d1aa1beafce63f5afb0ce064
CYCOABHI <s.abhirup@yahoo.com>
2015-11-25 19:31:20 -0500

    fix
    fix

--- Puppy Standalone Orbwalker/Program.cs
+++ Puppy Standalone Orbwalker/Program.cs
@@ -1,33 +1,61 @@
-﻿using System;
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using LeagueSharp;
 using LeagueSharp.Common;
+using SharpDX;
+using SPrediction;
 
-namespace PuppyStandAloneOrbwalker
+namespace PuppyStandaloneOrbwalker
 {
-    class StandaloneOrbwalker
+    internal class Program
     {
-
-        private Menu Config;
-
-        private Orbwalking.Orbwalker _orbwalker;
-
-        private StandaloneOrbwalker()
+        public const string ChampionName = "balblabll";
+        static Orbwalking.Orbwalker Orbwalker;
+        static Menu Menu;
+        public static Spell Q;
+        public static Spell W;
+        public static Spell E;
+        public static Spell R;
+        private static void Main(string[] args)
         {
-            CustomEvents.Game.OnGameLoad += OnGameLoad;
+            CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
         }
 
-        private void OnGameLoad(EventArgs args)
+        private static void Game_OnGameLoad(EventArgs args)
         {
-            Config = new Menu("PuppyStandalone Orbwalker", "PuppyStandalone Orbwalker", true);
-            Menu orbwalkerConfig = new Menu("Orbwalker", "orbwalker");
-            _orbwalker = new Orbwalking.Orbwalker(orbwalkerConfig);
-            Config.AddSubMenu(orbwalkerConfig);
-            Config.AddToMainMenu();
-        }
+            Menu = new Menu("PuppyStandaloneOrbwalker (OP)", ObjectManager.Player.ChampionName, true);
 
-        public static void Main(string[] args)
+            Menu OrbwalkerMenu = Menu.AddSubMenu(new Menu("Orbwalker", "Orbwalker"));
+            Orbwalker = new Orbwalking.Orbwalker(OrbwalkerMenu);
+
+            Menu TargetSelectorMenu = Menu.AddSubMenu(new Menu("Target Selector", "TargetSelector"));
+            TargetSelector.AddToMenu(TargetSelectorMenu);
+
+            Menu.AddToMainMenu();
+            Q = new Spell(SpellSlot.Q, 1);
+            W = new Spell(SpellSlot.W, 1);
+            E = new Spell(SpellSlot.E, 1);
+            R = new Spell(SpellSlot.R, 1);
+
+            Game.OnUpdate += Game_OnGameUpdate;
+
+        }
+        private static void Game_OnGameUpdate(EventArgs args)
         {
-            new StandaloneOrbwalker();
+            switch (Orbwalker.ActiveMode)
+            {
+                case Orbwalking.OrbwalkingMode.Combo:
+
+                    var Target = TargetSelector.GetTarget(Q.Range, TargetSelector.DamageType.Magical);
+
+                    if (Q.IsReady()) Q.Cast(Target);
+                    if (W.IsReady()) W.Cast(Target);
+                    if (E.IsReady()) E.Cast(Target);
+                    if (R.IsReady()) R.Cast(Target);
+                    break;
+            }
         }
     }
 }
