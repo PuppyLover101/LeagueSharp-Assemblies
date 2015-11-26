44fdae88da81b9d400b9c0af1422ff5744eabbd0
CYCOABHI <s.abhirup@yahoo.com>
2015-11-25 19:28:05 -0500

    fix

---
+++ Puppy Standalone Orbwalker/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using LeagueSharp;
+using LeagueSharp.Common;
+
+namespace PuppyStandAloneOrbwalker
+{
+    class StandaloneOrbwalker
+    {
+
+        private Menu Config;
+
+        private Orbwalking.Orbwalker _orbwalker;
+
+        private StandaloneOrbwalker()
+        {
+            CustomEvents.Game.OnGameLoad += OnGameLoad;
+        }
+
+        private void OnGameLoad(EventArgs args)
+        {
+            Config = new Menu("PuppyStandalone Orbwalker", "PuppyStandalone Orbwalker", true);
+            Menu orbwalkerConfig = new Menu("Orbwalker", "orbwalker");
+            _orbwalker = new Orbwalking.Orbwalker(orbwalkerConfig);
+            Config.AddSubMenu(orbwalkerConfig);
+            Config.AddToMainMenu();
+        }
+
+        public static void Main(string[] args)
+        {
+            new StandaloneOrbwalker();
+        }
+    }
+}
