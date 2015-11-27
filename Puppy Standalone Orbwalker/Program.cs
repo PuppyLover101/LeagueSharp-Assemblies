c9268c36ff2cc5d11cb3e64bc1f46ed3179b75d9
TheKushStyle <TheKushStyle@users.noreply.github.com>
2015-11-27 11:22:32 +0100

    clean

--- Puppy Standalone Orbwalker/Program.cs
+++ Puppy Standalone Orbwalker/Program.cs
@@ -10,13 +10,9 @@ namespace PuppyStandaloneOrbwalker
 {
     internal class Program
     {
-        public const string ChampionName = "balblabll";
         static Orbwalking.Orbwalker Orbwalker;
         static Menu Menu;
-        public static Spell Q;
-        public static Spell W;
-        public static Spell E;
-        public static Spell R;
+
         private static void Main(string[] args)
         {
             CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
@@ -29,32 +25,14 @@ namespace PuppyStandaloneOrbwalker
             Menu OrbwalkerMenu = Menu.AddSubMenu(new Menu("Orbwalker", "Orbwalker"));
             Orbwalker = new Orbwalking.Orbwalker(OrbwalkerMenu);
 
-            Menu TargetSelectorMenu = Menu.AddSubMenu(new Menu("Target Selector", "TargetSelector"));
-            TargetSelector.AddToMenu(TargetSelectorMenu);
-
             Menu.AddToMainMenu();
-            Q = new Spell(SpellSlot.Q, 1);
-            W = new Spell(SpellSlot.W, 1);
-            E = new Spell(SpellSlot.E, 1);
-            R = new Spell(SpellSlot.R, 1);
 
             Game.OnUpdate += Game_OnGameUpdate;
 
         }
         private static void Game_OnGameUpdate(EventArgs args)
         {
-            switch (Orbwalker.ActiveMode)
-            {
-                case Orbwalking.OrbwalkingMode.Combo:
-
-                    var Target = TargetSelector.GetTarget(Q.Range, TargetSelector.DamageType.Magical);
-
-                    if (Q.IsReady()) Q.Cast(Target);
-                    if (W.IsReady()) W.Cast(Target);
-                    if (E.IsReady()) E.Cast(Target);
-                    if (R.IsReady()) R.Cast(Target);
-                    break;
-            }
+            Orbwalker.SetAttack(true);
         }
     }
 }
